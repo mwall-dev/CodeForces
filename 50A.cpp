660c40892c855f55d5c15448c2686e46a7ec2eaa
mwall-dev <mwalchhuette@student.unimelb.edu.au>
2021-04-20 13:34:06 +1000

    Completed 50A

---
+++ 50A.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+/*
+
+    Notes:
+
+
+    Approach:
+        Just int divide by 2.
+
+
+
+
+
+*/
+
+int main() {
+    int m, n;
+
+    std::cin >> m >> n;
+
+    std::cout << (m*n)/2;
+    
+    return 0;
+}
